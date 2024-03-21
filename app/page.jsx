c38ea96016d5455ba2ccdb5013bd8f8ee5987224
tyson-tyler <benword25@gmail.com>
2024-03-21 17:33:27 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import Feed from "@/components/Feed";
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center pt-5 text-white">
+        Brower & Create {""}
+        <br className="max-md:hidden" />
+        <span className="usespan text-center ">Ai Prompts .</span>
+      </h1>
+      <p className="desc text-center text-gray-900">
+        Mypager is an open-source AI prompting tools for the Idea for your
+        project and get Solve your Problem .
+      </p>
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
