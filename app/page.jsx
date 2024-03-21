515f09bde5f59bdf8d607bb7a82960964ee2724e
tyson-tyler <benword25@gmail.com>
2024-03-21 18:35:35 +0530

    hello

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <h1 className="head_text text-center pt-5 text-white">
         Brower & Create {""}
         <br className="max-md:hidden" />
-        <span className="usespan text-center ">Ai Prompts .</span>
+        <span className="usespan text-center">Ai Prompts .</span>
       </h1>
       <p className="desc text-center text-gray-900">
         Mypager is an open-source AI prompting tools for the Idea for your
